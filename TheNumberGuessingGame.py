979a7a2b0987c0e012a39a931a5b43c77faab3cc
rich-caceres <richard.caceres22590@outlook.com>
2020-08-21 12:32:29 -0400

    Comments for clarity

--- TheNumberGuessingGame.py
+++ TheNumberGuessingGame.py
@@ -1,13 +1,17 @@
 import random
 
 #FOR TESTING randomNum = 100
+
+#Assigns the random number
 randomNum = random.randrange(-100, 101)
+#Gets user name
 userName= input('Please enter your name:\n')
 
 def numberSelectionGame(randomNum, userName):
     guesses= 0;
     userGuess= input('Hello ' + userName + ' a random number between -100 to 100 has been selected. Enter your guess:\n')
 
+    #Keeps track of guesses while loop ends once the user exceeds 7 number of guesses
     while guesses < 7:
         guesses = guesses + 1
         if int(userGuess) == randomNum:
@@ -19,19 +23,20 @@ def numberSelectionGame(randomNum, userName):
                 print('You guessed ' + userGuess + ' correctly in ' + str(guesses) + ' ' + guessStr + '.\nThank you '+ userName +' for playing!' )
             break;
         else:
+
+            #Hint generation begins here
             if int(userGuess)> 0 and randomNum < 0:
                 print('The random number is a negative number.')
             elif int(userGuess)< 0 and randomNum > 0:
                 print('The random number is a positive number.')
-                
+
+            #Next 6 lines generate the hint close hint as long as the user is within the range
             for x in range(randomNum-10, randomNum-5):    
                 if int(userGuess) == x or int(userGuess) == x:
                     print('Your guess is close to the secret number.')
             for x in range(randomNum-5, randomNum+1):
                 if int(userGuess) == x or int(userGuess) == x:
                     print('Your guess is very close to the secret number.')
-
-            
                 
             userGuess = input('you guessed '+ userGuess + ' which is incorrect.\nYou have ' + str(7 - guesses) + ' left.\n Please guess again:\n')
             if guesses == 7:
