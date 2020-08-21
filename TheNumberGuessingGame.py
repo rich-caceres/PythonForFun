f40561e2aa6da30cb4fc96187a5f903416c1f3d6
rich-caceres <richard.caceres22590@outlook.com>
2020-08-21 12:00:57 -0400

    Introducing hints to help the player choose correctly.

--- TheNumberGuessingGame.py
+++ TheNumberGuessingGame.py
@@ -1,7 +1,7 @@
 import random
 
 #FOR TESTING
-randomNum = 100
+randomNum = -100
 #randomNum = random.randrange(-100, 101)
 userName= input('Please enter your name:\n')
 
@@ -20,9 +20,15 @@ def numberSelectionGame(randomNum, userName):
                 print('You guessed ' + userGuess + ' correctly in ' + str(guesses) + ' ' + guessStr + '.\nThank you '+ userName +' for playing!' )
             break;
         else:
+            if int(userGuess) == randomNum-10 and randomNum > 0:
+                print('Your guess is close and the secret number is positive.')
+            elif int(userGuess) == randomNum+10 and randomNum < 0:
+                print('Your guess is close and the secret number is negative.')
             userGuess = input('you guessed '+ userGuess + ' which is incorrect.\nYou have ' + str(7 - guesses) + ' left.\n Please guess again:\n')
+            if guesses == 0:
+                print('The number that was generated is ' + str(randomNum) + ' thanks for playing ' + userName)
+            
     #FOR TESTING print(userGuess)
 
 numberSelectionGame(randomNum, userName)
-print('The number that was generated is ' + str(randomNum) + ' thanks for playing ' + userName)
 #FOR TESTING print(userName)
