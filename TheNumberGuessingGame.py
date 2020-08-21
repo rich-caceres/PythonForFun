1a321a0aa9423f47aa602dc600fe2c0b88468b79
rich-caceres <richard.caceres22590@outlook.com>
2020-08-21 12:09:03 -0400

    More hints added, fixed a logic issue.

--- TheNumberGuessingGame.py
+++ TheNumberGuessingGame.py
@@ -20,15 +20,22 @@ def numberSelectionGame(randomNum, userName):
                 print('You guessed ' + userGuess + ' correctly in ' + str(guesses) + ' ' + guessStr + '.\nThank you '+ userName +' for playing!' )
             break;
         else:
+            if int(userGuess)> 0 and randomNum < 0:
+                print('The random number is a negative number.')
+            elif int(userGuess)< 0 and randomNum > 0:
+                print('The random number is a positive number.')
+                
             if int(userGuess) == randomNum-10 and randomNum > 0:
                 print('Your guess is close and the secret number is positive.')
             elif int(userGuess) == randomNum+10 and randomNum < 0:
                 print('Your guess is close and the secret number is negative.')
+                
             userGuess = input('you guessed '+ userGuess + ' which is incorrect.\nYou have ' + str(7 - guesses) + ' left.\n Please guess again:\n')
-            if guesses == 0:
-                print('The number that was generated is ' + str(randomNum) + ' thanks for playing ' + userName)
+            if guesses == 7:
+                print('The number that was generated is ' + str(randomNum) + '. Thanks for playing ' + userName)
             
     #FOR TESTING print(userGuess)
 
 numberSelectionGame(randomNum, userName)
+
 #FOR TESTING print(userName)
