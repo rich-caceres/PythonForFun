3412e83f6d93fa9845cd7115c38bd136b2b48752
rich-caceres <richard.caceres22590@outlook.com>
2020-08-21 11:22:05 -0400

    Adding Python scripts

---
+++ RandomStrings.py
@@ -0,0 +1,11 @@
+print ("Hello there!", 5)
+print ("Hello there!\n", 5)
+print ("Hello there!\t", 5)
+str1="Hello"
+str2="there"
+str3="!"
+concatString= str1+" "+str2+str3
+scientificNotationNum= "{:.2e}".format(378)
+print(scientificNotationNum)
+print(chr(65))
+
