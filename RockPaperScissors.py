2fdb718a6251504c91ea8a91a9be4b0bce786f71
Richard Caceres <richard.caceres22590@outlook.com>
2020-09-01 17:07:58 -0400

    Fixed initial line sking the player to play. Jumping to a new line

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -6,7 +6,7 @@ loss = 0
 draw = 0
 
 
-answer=input('Would you like to play rock, paper, scissors? y/n')
+answer=input('Would you like to play rock, paper, scissors? y/n\n')
 
 def newPlayer():
     name = input('Enter your name to play!\n')
@@ -34,35 +34,35 @@ while True:
     # draw logic
     if computerChoice == 'Rock' and answer == 'r':
         draw = draw + 1
-        print('The computer chose ', computerChoice, ', its a draw.\n')
+        print('The computer chose ', computerChoice,', its a draw.\n')
     elif computerChoice == 'Scissors' and answer == 's':
         draw = draw + 1
-        print('The computer chose ', computerChoice, ', its a draw.\n') 
+        print('The computer chose ', computerChoice,', its a draw.\n') 
     elif computerChoice == 'Paper' and answer == 'p':
         draw = draw + 1
-        print('The computer chose ', computerChoice, ', its a draw.\n')
+        print('The computer chose ', computerChoice,', its a draw.\n')
 
     #loss logic
     if computerChoice == 'Rock' and answer == 's':
         loss = loss + 1
-        print('The computer chose ', computerChoice, ', you lost.\n')
+        print('The computer chose ', computerChoice,', you lost.\n')
     elif computerChoice == 'Scissors' and answer == 'p':
         loss = loss + 1
-        print('The computer chose ', computerChoice, ', you lost.\n')
+        print('The computer chose ', computerChoice,', you lost.\n')
     elif computerChoice == 'Paper' and answer == 'r':
         loss = loss + 1
-        print('The computer chose ', computerChoice, ', you lost.\n')
+        print('The computer chose ', computerChoice,', you lost.\n')
 
     #win logic
     if computerChoice == 'Rock' and answer == 'p':
         win = win+ 1
-        print('The computer chose ', computerChoice, ', you win!\n')
+        print('The computer chose ', computerChoice,', you win!\n')
     elif computerChoice == 'Scissors' and answer == 'r':
         win = win + 1
-        print('The computer chose ', computerChoice, ', you win!\n')
+        print('The computer chose ', computerChoice,', you win!\n')
     elif computerChoice == 'Paper' and answer == 's':
         win = win + 1
-        print('The computer chose ', computerChoice, ', you win!\n')
+        print('The computer chose ', computerChoice,', you win!\n')
 
     if answer == 'q':
         break
