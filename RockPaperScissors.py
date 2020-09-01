2d38997ee59af000c80eecbf41bb1de175cb09e3
Richard Caceres <richard.caceres22590@outlook.com>
2020-09-01 17:02:09 -0400

    Added all logic and record tracking for user

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,6 +1,6 @@
 import random
 
-choice= ['r','p','s']
+choice= ['Rock','Paper','Scissors']
 win = 0
 loss = 0
 draw = 0
@@ -9,20 +9,62 @@ draw = 0
 answer=input('Would you like to play rock, paper, scissors? y/n')
 
 def newPlayer():
-    name = input('Enter your name to play!')
+    name = input('Enter your name to play!\n')
     return name
 
-def computerChoice(choice):
+def randomChoice(choice):
     i = random.randrange(0,3)
     return choice[i]    
 
 def playerChoice():
-    playerChoice = input('Please choose between (r)ock, (p)aper, (s)cissors')
-    return playerChoice()
+    playerChoice = input('Please choose between (r)ock, (p)aper, (s)cissors or (q) to quit\n')
+    return playerChoice
 
 if answer == 'y':
     name = newPlayer()
 else:
     exit(0)
 
-computerChoice(choice)
+
+while True:
+    computerChoice = randomChoice(choice)
+    answer = playerChoice()
+
+        
+    # draw logic
+    if computerChoice == 'Rock' and answer == 'r':
+        draw = draw + 1
+        print('The computer chose ', computerChoice, ', its a draw.\n')
+    elif computerChoice == 'Scissors' and answer == 's':
+        draw = draw + 1
+        print('The computer chose ', computerChoice, ', its a draw.\n') 
+    elif computerChoice == 'Paper' and answer == 'p':
+        draw = draw + 1
+        print('The computer chose ', computerChoice, ', its a draw.\n')
+
+    #loss logic
+    if computerChoice == 'Rock' and answer == 's':
+        loss = loss + 1
+        print('The computer chose ', computerChoice, ', you lost.\n')
+    elif computerChoice == 'Scissors' and answer == 'p':
+        loss = loss + 1
+        print('The computer chose ', computerChoice, ', you lost.\n')
+    elif computerChoice == 'Paper' and answer == 'r':
+        loss = loss + 1
+        print('The computer chose ', computerChoice, ', you lost.\n')
+
+    #win logic
+    if computerChoice == 'Rock' and answer == 'p':
+        win = win+ 1
+        print('The computer chose ', computerChoice, ', you win!\n')
+    elif computerChoice == 'Scissors' and answer == 'r':
+        win = win + 1
+        print('The computer chose ', computerChoice, ', you win!\n')
+    elif computerChoice == 'Paper' and answer == 's':
+        win = win + 1
+        print('The computer chose ', computerChoice, ', you win!\n')
+
+    if answer == 'q':
+        break
+
+    print('This is your current score\nWins:', win,'\nLosses:', loss, '\nDraws: ', draw)
