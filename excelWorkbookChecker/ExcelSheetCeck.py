9c554fb202ffee81b791800a163d51dd5a0a9909
Richard Caceres <richard.caceres22590@outlook.com>
2021-01-26 13:16:52 -0500

    Excel workbook checker completed

--- excelWorkbookChecker/ExcelSheetCeck.py
+++ excelWorkbookChecker/ExcelSheetCeck.py
@@ -1,26 +1,39 @@
 import openpyxl
-#from openpyxl import load_workbook, Workbook
+import re
+from openpyxl import load_workbook
 
 
 def GetWorkBooks():
+    newWorkbook = openpyxl.Workbook()
+    ws= newWorkbook.active
+    
     workbook1 = openpyxl.load_workbook( 'ExcelFiles/' + input('Enter name of first excel file:\n')+ '.xlsx')
-    worksheet1 = workbook[input('Enter name of first worksheet:\n')]
+    worksheet1 = workbook1[input('Enter name of first worksheet:\n')]
 
     workbook2 = openpyxl.load_workbook('ExcelFiles/' + input('Enter name of second excel file:\n') + '.xlsx')
-    worksheet2 = workbook[input('Enter name of second worksheet\n')]
+    worksheet2 = workbook2[input('Enter name of second worksheet\n')]
+    rowNum = 1
     
-    for row1 in range(2, worksheet1.max_row+1):
-            sku1 = worksheet1['C' + str(row1)].value
+    for row1 in range(2, worksheet1.max_row):
+            sku1 = worksheet1['A' + str(row1)].value
+            sku1 = re.sub(".*\\s", "", sku1)
+            print(sku1)
             for row2 in range(2, worksheet2.max_row+1):
-                #TODO: have to strip SKU2 of initial B&D set by star sales
                 sku2 = worksheet2['C' + str(row2)].value
                 if(sku1 == sku2):
-                    #TODO: get the entire row that matches
-                    #TODO: send the row to addtoworkbook method below
-                    #AddtoWorkBook(row1)
-                break
+                    print(sku1 + " " + sku2 + " are the same. Starting the data entry process.")
+                    for nCol in range(1, worksheet2.max_column + 1): 
+                        rowData= worksheet2.cell(row= row2, column= nCol)
+                        ws.cell(row = rowNum, column= nCol).value = rowData.value
+                    print('Row has been entered')
+                    rowNum = rowNum + 1
+                        
+    newWorkbook.save('temp.xlsx')
+            
+                    
 
-def AddtoWorkbook(wsRow):
+#def AddtoWorkbook(wsRow, newWorkbook):
+    
     #TODO: create a new workbook so the program can use the cells
     #TODO: add the row to the new workbook
     #TODO: see the data being added to the workbook
