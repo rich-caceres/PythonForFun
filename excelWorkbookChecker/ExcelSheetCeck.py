4a2653f4102771d2f27d7aaa3493f16749a232ef
Richard Caceres <richard.caceres22590@outlook.com>
2021-01-26 10:06:08 -0500

    adding method to add to the new workbook

--- excelWorkbookChecker/ExcelSheetCeck.py
+++ excelWorkbookChecker/ExcelSheetCeck.py
@@ -8,13 +8,16 @@ def GetWorkBooks():
 
     workbook2 = openpyxl.load_workbook('ExcelFiles/' + input('Enter name of second excel file:\n') + '.xlsx')
     worksheet2 = workbook[input('Enter name of second worksheet\n')]
-    for row in range(2, worksheet.max_row+1):
-            sku1 = worksheet1['C' + str(row)].value
-            sku2 = 
-            break
+    for row1 in range(2, worksheet1.max_row+1):
+            sku1 = worksheet1['C' + str(row1)].value
+            for row2 in range(2, worksheet2.max_row+1):    
+                sku2 = worksheet2['C' + str(row2)].value
+                if(sku1 == sku2):
+                    #AddtoWorkBook(row1)
+                break
 
-def AddtoWorkbook():
-    
+def AddtoWorkbook(wsRow):
+    #TODO: create a new workbook so the program can use the cells
 
 if __name__ == '__main__':
 
