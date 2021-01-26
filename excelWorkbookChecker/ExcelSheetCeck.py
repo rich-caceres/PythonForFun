75d853591bc3770d6d128d40075cb6ca2bc94a6b
Richard Caceres <richard.caceres22590@outlook.com>
2021-01-26 09:57:49 -0500

    adding feature to check between both worksheets

--- excelWorkbookChecker/ExcelSheetCeck.py
+++ excelWorkbookChecker/ExcelSheetCeck.py
@@ -1,10 +1,20 @@
-from openpyxl import load_workbook, Workbook
+import openpyxl
+#from openpyxl import load_workbook, Workbook
 
 
 def GetWorkBooks():
-    workbook = load_workbook( 'ExcelFiles/' + input('Enter name of excel file:\n')+ '.xlsx')
-    worksheet = workbook[input('Enter name of worksheet:\n')]
+    workbook1 = openpyxl.load_workbook( 'ExcelFiles/' + input('Enter name of first excel file:\n')+ '.xlsx')
+    worksheet1 = workbook[input('Enter name of first worksheet:\n')]
 
+    workbook2 = openpyxl.load_workbook('ExcelFiles/' + input('Enter name of second excel file:\n') + '.xlsx')
+    worksheet2 = workbook[input('Enter name of second worksheet\n')]
+    for row in range(2, worksheet.max_row+1):
+            sku1 = worksheet1['C' + str(row)].value
+            sku2 = 
+            break
+
+def AddtoWorkbook():
+    
 
 if __name__ == '__main__':
 
