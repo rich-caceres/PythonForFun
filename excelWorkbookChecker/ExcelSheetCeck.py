59145c14a62db7404983d1da92111eab7fd1df35
Richard Caceres <richard.caceres22590@outlook.com>
2021-01-26 13:21:13 -0500

    Comments to explain the script's workflow

--- excelWorkbookChecker/ExcelSheetCeck.py
+++ excelWorkbookChecker/ExcelSheetCeck.py
@@ -4,30 +4,39 @@ from openpyxl import load_workbook
 
 
 def GetWorkBooks():
+    #creates new workbook to save rows in
     newWorkbook = openpyxl.Workbook()
     ws= newWorkbook.active
-    
+
+    #gets the first workbook that will be analyzed
     workbook1 = openpyxl.load_workbook( 'ExcelFiles/' + input('Enter name of first excel file:\n')+ '.xlsx')
     worksheet1 = workbook1[input('Enter name of first worksheet:\n')]
 
+    #gets second workbook to be analyzed against the first
     workbook2 = openpyxl.load_workbook('ExcelFiles/' + input('Enter name of second excel file:\n') + '.xlsx')
     worksheet2 = workbook2[input('Enter name of second worksheet\n')]
     rowNum = 1
-    
+
+    #starts a loop to grab the first sku
     for row1 in range(2, worksheet1.max_row):
             sku1 = worksheet1['A' + str(row1)].value
             sku1 = re.sub(".*\\s", "", sku1)
             print(sku1)
+
+            #second loop to check against the sku from the first loop
             for row2 in range(2, worksheet2.max_row+1):
                 sku2 = worksheet2['C' + str(row2)].value
                 if(sku1 == sku2):
                     print(sku1 + " " + sku2 + " are the same. Starting the data entry process.")
+
+                    #loop to get all the values from the columns
                     for nCol in range(1, worksheet2.max_column + 1): 
                         rowData= worksheet2.cell(row= row2, column= nCol)
                         ws.cell(row = rowNum, column= nCol).value = rowData.value
                     print('Row has been entered')
                     rowNum = rowNum + 1
-                        
+
+    #saves the workbook once we drop out of all the loops                    
     newWorkbook.save('temp.xlsx')
             
 
