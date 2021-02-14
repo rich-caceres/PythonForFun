f24f63deb65bd64b62118d10721c555cf0aa9884
Richard Caceres <richard.caceres22590@outlook.com>
2021-02-14 14:06:28 -0500

    Added feature to download pictures from a source, replaces name of picture to where pictures need to be uploaded

--- checkImportSheets/ImportChecker.py
+++ checkImportSheets/ImportChecker.py
@@ -1,4 +1,7 @@
 import openpyxl
+import wget
+import urllib
+from urllib.error import HTTPError
 from openpyxl import load_workbook
 
 toImportWb = openpyxl.load_workbook('ExcelSheets/' + input('Enter name of the sheet that is staged to be imported\n')+ '.xlsx')
@@ -7,28 +10,29 @@ toImportWs = toImportWb.active
 fromMagentoWb = openpyxl.load_workbook('ExcelSheets/' + input('Enter name of the sheet that was exported from Magento\n')+ '.xlsx')
 fromMagentoWs = fromMagentoWb.active
 
-
+url = ""
 for row1 in range(2, toImportWs.max_row):
     sku1 = toImportWs['A' + str(row1)].value
     print(sku1)
     toImportWs['N' + str(row1)].value = '0'
-
-    url = toImportWs['O' + str(row1)].value
-
-    #TODO: get Picture and save it as whatever name it is
-    #TODO: picture name will be derived from source name
-    
-    #TODO: Add feature to import pictures from source
-    for nCol in range(15, 20):
-        #TODO: add pictureName
-        toImportWs.cell(row = row1, column = nCol) = pictureName
+    #wget downloads pictures from the source
+    if (toImportWs['O' + str(row1)].value != None):
+        try:
+            if(toImportWs['O' + str(row1)].value != url):
+                url = toImportWs['O' + str(row1)].value
+                image_Filename = wget.download(url)
+        except urllib.error.HTTPError as exception:
+            print(exception)
+            image_Filename = ""
+            
+        for nCol in range(15, 20):
+            toImportWs.cell(row = row1, column = nCol).value = image_Filename
     
     for row2 in range(2, fromMagentoWs.max_row + 1):
         sku2 = fromMagentoWs['A' + str(row2)].value
         if(sku1 == sku2):
+            #adds data to the cells that will be imported to Magento
             if(fromMagentoWs['C' + str(row2)].value != 'Default' or fromMagentoWs['C' + str(row2)].value != None):
-                #Add feature to take the data in the cell and replace
-                #data in the cell of excel sheet that will be imported
                 toImportWs['C' + str(row1)].value = fromMagentoWs['C' + str(row2)].value
                 
                 if(fromMagentoWs['E' + str(row2)].value != None):
