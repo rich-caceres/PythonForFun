f5e8f6d13a9b74b6940cca2e28b0b2e5182da242
Richard Caceres <richard.caceres22590@outlook.com>
2021-03-24 08:44:18 -0400

    adding a method in class to return colunms for use by openpy

--- new_product_conversions/app/Classes/ExcelSheetObject.py
+++ new_product_conversions/app/Classes/ExcelSheetObject.py
@@ -1,14 +1,21 @@
-import openpyxl
-import re
-from openpyxl import load_workbook, Workbook
-
 class ExcelSheet:
     
     def __init__(self, sheet_name):
 
-        self.sheet_name = sheet_name
+        try:
+            self.sheet_name = sheet_name
+        except:
+            print("An error ocurred")
 
+    def returnCols(self):
+
+        col = input("The column letter for Python:\n")
+
+        return col
+            
 if __name__ == '__main__':
 
     sheet = ExcelSheet(input('Please enter excel sheet name:\n'))
+    col = sheet.returnCols()
     print(sheet.sheet_name)
+    print(col)
