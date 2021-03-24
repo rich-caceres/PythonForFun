e015a1e46a6ea2aa42142e450141325ab229d650
Richard Caceres <richard.caceres22590@outlook.com>
2021-03-24 09:14:17 -0400

    added the method for col return to app main file

--- new_product_conversions/app/app.py
+++ new_product_conversions/app/app.py
@@ -4,6 +4,14 @@ from openpyxl import load_workbook, Workbook
 from Classes.ExcelSheetObject import ExcelSheet
 
 if __name__ == '__main__':
-
+    
     sheet = ExcelSheet(input('Please enter excel sheet name:\n'))
+
+    #following method will return column letters
+    col = sheet.returnCols()
+    '''write a while loop to allow the user to choose the total amount of cols
+    that they would like to enter to dynamically chose the total amount of cols.
+    May need to create a tuple to do this! Look into Tuples again.
+    '''
+    
     print(sheet.sheet_name)
