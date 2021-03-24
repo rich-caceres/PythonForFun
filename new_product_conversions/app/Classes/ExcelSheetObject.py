3d4c2f4b341d09afa055cc977b24545d3803e1c2
Richard Caceres <richard.caceres22590@outlook.com>
2021-03-24 09:34:27 -0400

    added a method to return total cols

--- new_product_conversions/app/Classes/ExcelSheetObject.py
+++ new_product_conversions/app/Classes/ExcelSheetObject.py
@@ -1,21 +1,22 @@
 class ExcelSheet:
     
     def __init__(self, sheet_name):
-
         try:
             self.sheet_name = sheet_name
         except:
             print("An error ocurred")
 
+    def totalCols(self):
+        return input("how many columns do you want to check?\n")
+
     def returnCols(self):
+        return input("The column letter for Python:\n")
 
-        col = input("The column letter for Python:\n")
-
-        return col
             
 if __name__ == '__main__':
 
     sheet = ExcelSheet(input('Please enter excel sheet name:\n'))
     col = sheet.returnCols()
+    totalCols = sheet.totalCols()
     print(sheet.sheet_name)
     print(col)
