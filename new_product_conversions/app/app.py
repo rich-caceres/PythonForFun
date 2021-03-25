7b3acc33b7d954cd168c4498d18c60e5e10d9bb1
Richard Caceres <richard.caceres22590@outlook.com>
2021-03-25 11:51:08 -0400

    Script cycles through all available sheets and copies data to new workbook and sheets in workbook

--- new_product_conversions/app/app.py
+++ new_product_conversions/app/app.py
@@ -7,17 +7,16 @@ from Classes.ExcelSheetObject import ExcelSheet
 if __name__ == '__main__':
 
     wb = openpyxl.Workbook()
-    ws = wb.active
     
     while True:
         try:
             sheet = ExcelSheet(input('Please enter excel sheet name:\n'), input('Enter Sku Identification to add to beginning of SKU:\n'), input('Enter starting row:\n'))
             workbook = openpyxl.load_workbook('../ExcelFiles/' + sheet.sheet_name)
-            multiSheet = input("Are there multiple sheets? y/n\n")
-            if(multiSheet == "Y"):
-                worksheet = workbook[input('What is the name of the worksheet to target?\n')]
-            else:
-                worksheet = workbook.active
+            #multiSheet = input("Are there multiple sheets? y/n\n")
+            #if(multiSheet == "Y"):
+                #worksheet = workbook[input('What is the name of the worksheet to target?\n')]
+            #else:
+                #worksheet = workbook.active
             break
         except:
             answer = input("This workbook was not found! try again, Enter Q to quit the program, otherwise hit enter to continue.\n")
@@ -34,15 +33,19 @@ if __name__ == '__main__':
     for i in range(1,numOfCols):
         colDiction['col{0}'.format(i)] = sheet.returnCols()
 
-    for i in range(1,numOfCols):
-        rowForWs = 1
-        for row in range(sheet.starting_row, worksheet.max_row + 1):
-            ws.cell(row = rowForWs, column = i).value = worksheet[colDiction['col{0}'.format(i)] + str(row)].value
-            rowForWs += 1
+    for sheets in workbook.worksheets:
+        worksheet = sheets
+        ws = wb.create_sheet(worksheet.title)
+        for i in range(1,numOfCols):
+            rowForWs = 1
+            for row in range(sheet.starting_row, worksheet.max_row + 1):
+                ws.cell(row = rowForWs, column = i).value = worksheet[colDiction['col{0}'.format(i)] + str(row)].value
+                rowForWs += 1
 
-    for i in range(1, ws.max_row + 1):
-        fixedSku = sheet.manu_ident + " " + str(ws.cell(row = i, column = 1).value)
-        ws.cell(row=i, column = 1).value = fixedSku
+    for ws in wb.worksheets:
+        for i in range(1, ws.max_row + 1):
+            fixedSku = sheet.manu_ident + " " + str(ws.cell(row = i, column = 1).value)
+            ws.cell(row=i, column = 1).value = fixedSku
 
     wb.save('testSheet.xlsx')       
     print(sheet.sheet_name)
