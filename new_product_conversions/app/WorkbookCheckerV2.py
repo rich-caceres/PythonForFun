f1dbe13ec9c043fb746017847e4b6f71a529a3fc
Richard Caceres <richard.caceres22590@outlook.com>
2021-04-21 20:12:24 -0400

    fixed issue where iteration was not working

--- new_product_conversions/app/WorkbookCheckerV2.py
+++ new_product_conversions/app/WorkbookCheckerV2.py
@@ -7,30 +7,34 @@ from Classes.ExcelSheetObject import ExcelSheet
 
 def workbookCheck():
 
+    upsell_products = {}
+    columnLetter = None
     while True:
         try:
             magentoSheet = ExcelSheet(input('Please enter the name of the Magento exported excel sheet:\n'), '', input('Enter starting row:\n'))
             excelSheetToCheck = ExcelSheet(input('Please enter the name of the excel sheet to check:\n'), '', input('Enter starting row:\n'))
-            mageXcelSheet = openpyxl.load_workbook('../ExcelFiles/' + magentoSheet.sheet_name)
-            XcelSheetToCheck = openpyxl.load_workbook('../ExcelFiles/' + excelSheetToCheck.sheet_name)
+            mageXcelWorkbook = openpyxl.load_workbook('../ExcelFiles/' + magentoSheet.sheet_name)
+            mageXcelSheet = mageXcelWorkbook.active
+            XcelWorkbook = openpyxl.load_workbook('../ExcelFiles/' + excelSheetToCheck.sheet_name)
+            XcelWorksheet = XcelWorkbook.active
             break
         except:
             answer = input("This work book was not found! press enter to try again. Enter the letter Q to quit the program")
             if(answer == 'Q'):
                 sys.exit('You have quit the program!')
 
-        upsell_products = {}
 
-    for row in range(excelSheetToCheck.starting_row, XcelSheetToCheck.max_row + 1):
-        columnLetter = input('Enter column that contains the Sku for each product')
-        Sku1 = XcelSheetToCheck['B' + str(row)].value
+    for row in range(excelSheetToCheck.starting_row, XcelWorksheet.max_row + 1):
+        if columnLetter is None:
+            columnLetter= input('Please enter the column letter containing SKU on the sheet the contains the cross sell items:\n')
+        Sku1 = XcelWorksheet['B' + str(row)].value
 
         for row1 in range(magentoSheet.starting_row, mageXcelSheet.max_row + 1):
-            Sku2 = mageXcelSheet['A' + str(row)].value
+            Sku2 = str(mageXcelSheet['A' + str(row1)].value)
             Sku2 = re.sub(".*\\s", "", Sku2)
-            
             if(Sku1 == Sku2):
                 print('Entered the if statement')
+                break
                         
     """ 
                 if Sku1 matches Sku2:
