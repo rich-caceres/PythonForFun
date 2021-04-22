79a9cc616faea8c2fddeee196616c9077bb1945a
Richard Caceres <richard.caceres22590@outlook.com>
2021-04-22 11:52:42 -0400

    Creates dictionary of upsell products, adds Manufacturere ident at the front of each sku

--- new_product_conversions/app/WorkbookCheckerV2.py
+++ new_product_conversions/app/WorkbookCheckerV2.py
@@ -7,12 +7,11 @@ from Classes.ExcelSheetObject import ExcelSheet
 
 def workbookCheck():
 
-    upsell_products = {}
     columnLetter = None
     while True:
         try:
             magentoSheet = ExcelSheet(input('Please enter the name of the Magento exported excel sheet:\n'), '', input('Enter starting row:\n'))
-            excelSheetToCheck = ExcelSheet(input('Please enter the name of the excel sheet to check:\n'), '', input('Enter starting row:\n'))
+            excelSheetToCheck = ExcelSheet(input('Please enter the name of the excel sheet to check:\n'), input('Enter initial identifier for manufacturer:\n'), input('Enter starting row:\n'))
             mageXcelWorkbook = openpyxl.load_workbook('../ExcelFiles/' + magentoSheet.sheet_name)
             mageXcelSheet = mageXcelWorkbook.active
             XcelWorkbook = openpyxl.load_workbook('../ExcelFiles/' + excelSheetToCheck.sheet_name)
@@ -24,21 +23,23 @@ def workbookCheck():
                 sys.exit('You have quit the program!')
 
 
-    for row in range(excelSheetToCheck.starting_row, XcelWorksheet.max_row + 1):
+    for row1 in range(excelSheetToCheck.starting_row, XcelWorksheet.max_row + 1):
         if columnLetter is None:
             columnLetter= input('Please enter the column letter containing SKU on the sheet the contains the cross sell items:\n')
-        Sku1 = XcelWorksheet['B' + str(row)].value
-
-        for row1 in range(magentoSheet.starting_row, mageXcelSheet.max_row + 1):
-            Sku2 = str(mageXcelSheet['A' + str(row1)].value)
+        Sku1 = XcelWorksheet['B' + str(row1)].value
+        upsell_products = {}
+        for row2 in range(magentoSheet.starting_row, mageXcelSheet.max_row + 1):
+            Sku2 = str(mageXcelSheet['A' + str(row2)].value)
             Sku2 = re.sub(".*\\s", "", Sku2)
             if(Sku1 == Sku2):
-                print('Entered the if statement')
+                print(f'Entered if Statement {Sku1} == {Sku2}')
+                for cols in range(5, XcelWorksheet.max_column+1):
+                    if XcelWorksheet.cell(row = row1, column = cols).value is not None:
+                        upsell_products['{0}'.format(cols)] =excelSheetToCheck.manu_ident + " " + XcelWorksheet.cell(row=row1, column=cols).value
+                print(upsell_products)
                 break
                         
     """ 
-                if Sku1 matches Sku2:
-                    create dictionary of upsell_products
         loop through dictionary of upsell_products:        
             if dictionary of upsell products not in mageXcelSheet:
                 delete the sku from dictionary
